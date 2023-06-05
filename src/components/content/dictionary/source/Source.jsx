9cbcd79fd4418ac142af183a270c75c0f94870e6
JubbaTheHutt <vladisabr@gmail.com>
2023-06-05 13:46:57 +0300

    link fix 2

--- src/components/content/dictionary/source/Source.jsx
+++ src/components/content/dictionary/source/Source.jsx
@@ -3,21 +3,23 @@ import * as PropTypes from 'prop-types';
 
 const Source = (props) => {
     return (
-        <div className={'pb-32'}>
+        <div className={'pb-32 max-w-full'}>
             <div
                 className={'border-[1px] my-4 mb-8 w-full grow-1 bg-[#f4f4f4]'}
             ></div>
             <div className={'flex gap-8'}>
                 <span className={'text-[#9e9e9e]'}>Source</span>
-                <a
-                    className={
-                        'underline flex flex-wrap overflow-hidden justify-center items-center gap-2'
-                    }
-                    href={props.response?.sourceUrls[0]}
-                >
-                    {props.response?.sourceUrls[0]}
-                    <LuExternalLink />
-                </a>
+                <div>
+                    <a
+                        className={
+                            'underline flex overflow-hidden justify-center items-center gap-2'
+                        }
+                        href={props.response?.sourceUrls[0]}
+                    >
+                        {props.response?.sourceUrls[0]}
+                        <LuExternalLink />
+                    </a>
+                </div>
             </div>
         </div>
     );
