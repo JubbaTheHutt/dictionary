112e896e81562cdc6ad8663700bbda10f33b67fe
JubbaTheHutt <vladisabr@gmail.com>
2023-06-05 13:58:40 +0300

    link fix 3

--- src/components/content/dictionary/source/Source.jsx
+++ src/components/content/dictionary/source/Source.jsx
@@ -3,16 +3,16 @@ import * as PropTypes from 'prop-types';
 
 const Source = (props) => {
     return (
-        <div className={'pb-32 max-w-full'}>
+        <div className={'pb-32'}>
             <div
                 className={'border-[1px] my-4 mb-8 w-full grow-1 bg-[#f4f4f4]'}
             ></div>
             <div className={'flex gap-8'}>
                 <span className={'text-[#9e9e9e]'}>Source</span>
-                <div>
+                <div className={'max-w-[60%]'}>
                     <a
                         className={
-                            'underline flex overflow-hidden justify-center items-center gap-2'
+                            'underline flex justify-start items-center gap-2'
                         }
                         href={props.response?.sourceUrls[0]}
                     >
